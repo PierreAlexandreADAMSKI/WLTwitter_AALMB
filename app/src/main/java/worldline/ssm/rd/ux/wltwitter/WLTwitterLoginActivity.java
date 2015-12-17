48e87bc2e5f4a6e3e389eb9ec6f4c3b66fcf31fb
Pierre-Alexandre ADAMSKI <pierre-alexandre.adamski@isen-lille.fr>
2015-12-17 01:18:16 +0100

    new style edit : now EditText cursors are only appearing when selected. (see WLTwitterLoginActivity -> onCreate()). [works on emulator but not on phone?]

--- app/src/main/java/worldline/ssm/rd/ux/wltwitter/WLTwitterLoginActivity.java
+++ app/src/main/java/worldline/ssm/rd/ux/wltwitter/WLTwitterLoginActivity.java
@@ -3,10 +3,14 @@ package worldline.ssm.rd.ux.wltwitter;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.text.Selection;
+import android.text.Spannable;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -39,31 +43,67 @@ public class WLTwitterLoginActivity extends Activity implements View.OnClickList
          */
         setContentView(R.layout.activity_login);
         findViewById(R.id.loginButton).setOnClickListener(this);
+        // cast the String as an EditText for compatibility
+        login = (EditText) this.findViewById(R.id.loginEditText);
+        //login.setCursorVisible(true);
+        password = (EditText) this.findViewById(R.id.passwordEditText);
+        //password.setCursorVisible(true);
+
+        saveLoginCheckBox = (CheckBox)findViewById(R.id.saveLoginCheckBox);
 
         //if remember me was already checked then show login and password and check the box
         if (!PreferenceUtils.getCheckBox()) {
             saveLoginCheckBox.setChecked(true);
         }
+
+        /* -- SET CURSOR ONLY VISIBLE WHEN KEYBOARD LAUNCHES -- */
+        //create a view of the login container activity
+        final View loginActivity = findViewById(R.id.login_activity);
+        //get the tree view and set a listener on the layout from the view
+        loginActivity.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            //set up comportment when the view updates threw the listener
+            @Override
+            public void onGlobalLayout() {
+                //on all of the container area create a rectangle off same height and width
+                Rect r = new Rect();
+                loginActivity.getWindowVisibleDisplayFrame(r);
+                //check if the rectangle height shrinks like when keyboard launches
+                //for than check on the difference between regular container's height and
+                //updated container's height when a system item appears
+                int heightDiff = loginActivity.getRootView().getHeight() - (r.bottom - r.top);
+                if (login != null && password != null) {
+                    // if more than 100 pixels, its probably a keyboard...
+                    if (heightDiff > 100) {
+                        //then set cursor visible for login and password
+                        login.setCursorVisible(true);
+                        password.setCursorVisible(true);
+                    } else {
+                        //else set both cursors invisible
+                        //cursor will appear when item is selected only
+                        //so when keyboard launches
+                        login.setCursorVisible(false);
+                        password.setCursorVisible(false);
+                    }
+                }
+            }
+        });
     }
 
-    /**
-     * @param v type : View
-     *          We verify in the onClick method that login and password fields are not Empty
-     *          We print it with a Toast. It's a message which is printed on the screen
-     *          in a short time. [It's also used for debugging by the developpers
-     *          <p/>
-     *          To print the text, we use the static makeText method and the show method implemented in
-     *          the Toast class. makeText will construct the message as a toast and show will print it
-     *          makeText(Context context, CharSequence text, int duration)
+
+                /**
+                 * @param v type : View
+                 *          We verify in the onClick method that login and password fields are not Empty
+                 *          We print it with a Toast. It's a message which is printed on the screen
+                 *          in a short time. [It's also used for debugging by the developpers
+                 *          <p/>
+                 *          To print the text, we use the static makeText method and the show method implemented in
+                 *          the Toast class. makeText will construct the message as a toast and show will print it
+                 *          makeText(Context context, CharSequence text, int duration)
      */
     @Override
     public void onClick(View v) {
         // get the login from id loginEditText on the activity_main.xml
-        // cast the String as an EditText for compatibility
-        login = (EditText) this.findViewById(R.id.loginEditText);
         Log.i("onClick(View) - user login ",login.getText().toString());
-        password = (EditText) this.findViewById(R.id.passwordEditText);
-        saveLoginCheckBox = (CheckBox)findViewById(R.id.saveLoginCheckBox);
 
         if (TextUtils.isEmpty(login.getText().toString())) {
             // if login is empty throw a toast
